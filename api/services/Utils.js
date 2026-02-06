67f8f483d6fc053cf4468de43abc83b9a4da6761
uchida0627 <97079120+uchida0627@users.noreply.github.com>
2026-02-06 15:12:14 +0900

    Update Utils.js
    change Filter function

--- api/services/Utils.js
+++ api/services/Utils.js
@@ -11,13 +11,22 @@ module.exports.GetDataSet = async function () {
     });
 };
 
-module.exports.Filter = async function (updates, objects) {
-    var objectstmp = objects.sort();
-    var objectsRevert = objectstmp.reverse();
-    var resultFilter = objectsRevert;
-    updates.push(resultFilter);
-    return updates;
-};
+
++ module.exports.Filter = function (objects) {
++     // 元配列を壊さずに逆順（必要ならソート基準を明示）
++     return [...objects].reverse();
++ };
+
+// -----
+// original Filter
+// -----
+// module.exports.Filter = async function (updates, objects) {
+//   // var objectstmp = objects.sort();
+//     // var objectsRevert = objectstmp.reverse();
+//     // var resultFilter = objectsRevert;
+//     // updates.push(resultFilter);
+//     // return updates;
+// };
 
 module.exports.GetNumber = function () {
     return getNumber(1,5000);
